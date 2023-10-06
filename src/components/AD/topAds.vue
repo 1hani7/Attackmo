7d644c52093bfa33cd7fe0bb9d8a3132b46b045a
1hani7 <1hani7@naver.com>
2023-10-06 23:40:40 +0900

    adTopHeightFix

--- src/components/AD/topAds.vue
+++ src/components/AD/topAds.vue
@@ -1,36 +1,92 @@
 <template>
-    <div id="pc"><img :src="pcPath" alt=""></div>
+    <div id="parentDiv" :style="{ backgroundColor: backgroundColor }">
+        <img id="childImg" :src="pcPath" @load="findMostFrequentColor">
+    </div>
 </template>
 
 <script>
-import {ref, onMounted} from 'vue';
-export default{
-    name:'topAds',
-    setup(){
+import { ref, onMounted } from 'vue';
+export default {
+    name: 'topAds',
+    setup() {
         const pcPath = ref('');
         const tabletPath = ref('');
         const mobilePath = ref('');
 
-        onMounted(()=>{
-            const random = Math.floor(Math.random()*5)+1;
-            pcPath.value = '/src/images/topAd/topAd'+random+'.png'
+        const backgroundColor = ref('white'); // 초기 배경색
+
+        const findMostFrequentColor = (event) => {
+            const img = event.target;
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
+            canvas.width = img.width;
+            canvas.height = img.height;
+            context.drawImage(img, 0, 0, img.width, img.height);
+
+            const imageData = context.getImageData(0, 0, img.width, img.height).data;
+
+            // 색상 히스토그램 생성
+            const colorCounts = {};
+
+            for (let i = 0; i < imageData.length; i += 4) {
+                const red = imageData[i];
+                const green = imageData[i + 1];
+                const blue = imageData[i + 2];
+                const rgb = `rgb(${red},${green},${blue})`;
+
+                if (colorCounts[rgb]) {
+                    colorCounts[rgb]++;
+                } else {
+                    colorCounts[rgb] = 1;
+                }
+            }
+
+            // 가장 많이 분포된 색상 찾기
+            let mostFrequentColor = 'white'; // 기본값 설정
+            let maxCount = 0;
+
+            for (const color in colorCounts) {
+                if (colorCounts[color] > maxCount) {
+                    mostFrequentColor = color;
+                    maxCount = colorCounts[color];
+                }
+            }
+
+            backgroundColor.value = mostFrequentColor;
+        };
+
+        onMounted(() => {
+            const random = Math.floor(Math.random() * 5) + 1;
+            pcPath.value = '/src/images/topAd/topAd' + random + '.png'
+
+            const img = document.getElementById('childImg');
+            img.addEventListener('load', findMostFrequentColor);
         })
 
-        return {pcPath, tabletPath, mobilePath}
+        return { pcPath, tabletPath, mobilePath, backgroundColor }
     }
 }
 </script>
 
 <style scoped>
-div{
-    width:100%;
+div {
+    height: 90px;
 }
-@media(max-width:1194px){
-    div{display:none;}
+
+div img {
+    width: fit-content;
+    height: 90px;
 }
-img{
-    position: relative; left:50%;
-    transform:translateX(-50%);
-    width:100%;
+
+@media(max-width:1194px) {
+    div {
+        display: none;
+    }
 }
-</style>
+
+img {
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 100%;
+}</style>
