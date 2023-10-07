b0a494c5361c2e78e4f21cac0c3506f299454390
1hani7 <1hani7@naver.com>
2023-10-07 17:43:25 +0900

    yguiv

--- src/components/chart/lineChart.vue
+++ src/components/chart/lineChart.vue
@@ -5,54 +5,61 @@
 </template>
 
 <script>
-import { Chart } from 'chart.js/auto';
+import { Chart} from 'chart.js/auto';
+import {onMounted} from 'vue'
 
 export default {
     name: 'lineChart',
     setup() {
 
-        window.addEventListener('scroll', function(){
-            const chartWrap = document.getElementById('chartWrap');
-            const rect = chartWrap.getBoundingClientRect();
-            if (rect.top < window.innerHeight && rect.bottom >= 0){
-                chartMaker();
-            }
-        })
+        onMounted(()=>{
 
-        const chartMaker = () => {
-            const ctx = document.getElementById('chart');
-            const label = ['09.03', '09.04', '09.05','09.06', '09.07', '09.08', '09.09', '09.10', '09.11', '09.12', ];
-            const data = [80000, 21000, 20000, 19000, 15000, 55000, 45000, 11000, 10000, 16000];
-            const chart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: label,
-                    datasets: [{
-                        label: label,
-                        data: data,
-                        borderWidth: 3,
-                        borderColor: '#F9C041',
-                        pointBorderWidth: 0,
-                        pointStyle: false,
-                        fill:true,
-                        backgroundColor: '#F9C04180'
-                    }]
-                },
-                options: {
-                    animation: {
-                        duration: 2000,
-                        easing: 'easeInOutQuad'
+            window.addEventListener('scroll', function () {
+                const chartWrap = document.getElementById('chartWrap');
+                const rect = chartWrap.getBoundingClientRect();
+                if (rect.top < window.innerHeight && rect.bottom >= 0) {
+                    chartMaker();
+                }
+            })
+
+            const chartMaker = () => {
+                const ctx = document.getElementById('chart');
+                const label = ['09.03', '09.04', '09.05', '09.06', '09.07', '09.08', '09.09', '09.10', '09.11', '09.12',];
+                const data = [80000, 21000, 20000, 19000, 15000, 55000, 45000, 11000, 10000, 16000];
+                const chart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: label,
+                        datasets: [{
+                            label: label,
+                            data: data,
+                            borderWidth: 3,
+                            borderColor: '#F9C041',
+                            pointBorderWidth: 0,
+                            pointStyle: false,
+                            fill: true,
+                            backgroundColor: '#F9C04180'
+                        }]
                     },
-                    responsive: true,
-                    maintainAspectRatio: false,
-                    plugins:{
-                        legend:{
-                            display:false
+                    options: {
+                        animation: {
+                            duration: 2000,
+                            easing: 'easeInOutQuad'
+                        },
+                        responsive: true,
+                        maintainAspectRatio: false,
+                        plugins: {
+                            legend: {
+                                display: false
+                            }
                         }
                     }
-                }
-            });
-        }
+                });
+            }
+
+        })
+
+        
 
         return {  };
     }
--- src/components/chart/radarChart.vue
+++ src/components/chart/radarChart.vue
@@ -6,85 +6,94 @@
 
 <script>
 import { Chart } from 'chart.js/auto';
+import { onMounted } from 'vue'
 
 export default {
     name: 'radarChart',
     setup() {
 
-        window.addEventListener('scroll', function(){
-            const chartWrap = document.getElementById('chartWrap');
-            const rect = chartWrap.getBoundingClientRect();
-            if (rect.top < window.innerHeight && rect.bottom >= 0){
-                chartMaker();
+        onMounted(() => {
+            const ctx = document.getElementById('radar');
+            console.log(ctx)
+
+            window.addEventListener('scroll', function () {
+                const chartWrap = document.getElementById('chartWrap');
+                const rect = chartWrap.getBoundingClientRect();
+                const ctx = document.getElementById('radar');
+                if (rect.top < window.innerHeight && rect.bottom >= 0 ) {
+                    chartMaker();
+                }
+            })
+
+            const chartMaker = () => {
+                const ctx = document.getElementById('radar');
+                const label = ['작품성', '음악', '연출력', '연기력', '오락성'];
+                const data = [5, 4.3, 4, 5, 3.4];
+                const myChart = new Chart(ctx, {
+                    type: 'radar',
+                    data: {
+                        labels: label,
+                        datasets: [{
+                            data: data,
+                            borderWidth: 0,
+                            backgroundColor: 'rgba(249, 192, 65, 0.8)',
+                            pointBorderWidth: 0,
+                            pointStyle: false,
+                            pointLabel: false
+                        }]
+                    },
+                    options: {
+                        animation: {
+                            duration: 2000,
+                            easing: 'easeInOutQuad'
+                        },
+                        responsive: true,
+                        maintainAspectRatio: false,
+                        scales: {
+                            r: {
+                                pointLabels: {
+                                    font: {
+                                        size: 17
+                                    }
+                                },
+                                grid: {
+                                    color: 'black'
+                                },
+                                angleLines: {
+                                    display: false
+                                },
+                                suggestedMin: 0,
+                                suggestedMax: 5,
+                                ticks: {
+                                    stepSize: 1,
+                                    display: false
+                                }
+                            }
+                        },
+                        plugins: {
+                            legend: {
+                                display: false
+                            }
+                        }
+                    }
+                });
             }
         })
 
-        const chartMaker = () => {
-            const ctx = document.getElementById('radar');
-            const label = ['작품성', '음악', '연출력', '연기력', '오락성'];
-            const data = [5, 4.3, 4, 5, 3.4];
-            const myChart = new Chart(ctx, {
-                type: 'radar',
-                data: {
-                    labels: label,
-                    datasets: [{
-                        data: data,
-                        borderWidth: 0,
-                        backgroundColor: 'rgba(249, 192, 65, 0.8)',
-                        pointBorderWidth: 0,
-                        pointStyle: false,
-                        pointLabel: false
-                    }]
-                },
-                options: {
-                    animation: {
-                        duration: 2000,
-                        easing: 'easeInOutQuad'
-                    },
-                    responsive: true,
-                    maintainAspectRatio: false,
-                    scales: {
-                        r: {
-                            pointLabels: {
-                                font: {
-                                    size: 17
-                                }
-                            },
-                            grid: {
-                                color:'black'
-                            },
-                            angleLines:{
-                                display: false
-                            },
-                            suggestedMin: 0,
-                            suggestedMax: 5,
-                            ticks: {
-                                stepSize: 1,
-                                display:false
-                            }
-                        }
-                    },
-                    plugins: {
-                        legend: {
-                            display: false
-                        }
-                    }
-                }
-            });
-        }
 
 
-        return {  };
+        return {};
     }
 }
 </script>
 
 <style scoped>
 #chartWrap {
-    width: 100%; 
+    width: 100%;
 }
 
 #chart {
-    width: 400px; height:400px;
+    width: 400px;
+    height: 400px;
 }
 </style>
