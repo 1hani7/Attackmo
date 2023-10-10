81c600b3bd5afdc0b53d0cb3cddfd7c1673dcaf6
yerim-je <a01023041704@gmail.com>
2023-10-10 11:11:24 +0900

    info

--- src/assets/js/info.js
+++ src/assets/js/info.js
@@ -1,10 +1,12 @@
  export default function() {
+  if(max-width >= 1194){
     $(".rec_movie").on("mouseover", function () {
       $(this).find(".summary").show();
     });
     $(".rec_movie").on("mouseout", function () {
       $(this).find(".summary").hide();
     });
+  }
   };
 
 
