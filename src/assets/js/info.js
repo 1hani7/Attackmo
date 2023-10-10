e352f18321d4b0b34dbc3134b82c35df63b45e26
yerim-je <a01023041704@gmail.com>
2023-10-10 12:02:08 +0900

    fsdfs

--- src/assets/js/info.js
+++ src/assets/js/info.js
@@ -1,12 +1,10 @@
  export default function() {
-  if(max-width >= 1194){
     $(".rec_movie").on("mouseover", function () {
       $(this).find(".summary").show();
     });
     $(".rec_movie").on("mouseout", function () {
       $(this).find(".summary").hide();
     });
-  }
   };
 
 
