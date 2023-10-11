65c87d87b1f3397ccd17968ce90ed2651534b049
yerim-je <a01023041704@gmail.com>
2023-10-11 10:46:32 +0900

    more comment

--- src/views/Rread.vue
+++ src/views/Rread.vue
@@ -65,7 +65,8 @@
                         <input type="text" placeholder="바른 말 고운 말, 함께 만드는 댓글 문화!">
                         <button class="reply_bt">등록하기</button>
                     </div>
-                    <div class="reply" v-for="(reply, index) in replies" :key="index">
+                    <div id="moreComment" @click="loadMore">이전 댓글 더보기 <i class="bi bi-chat-dots"></i></div>
+                    <div class="reply" v-for="(reply, index) in displayedReplies" :key="index">
                         <div class="reply_info">
                             <div class="nickName">{{ reply.nickName }}</div>
                             <div class="wDate">{{ reply.date }}</div>
@@ -78,7 +79,7 @@
                         <div class="reply_text">{{ reply.text }}</div>
                     </div>
                     <div class="mReply_wrap">
-                        <div class="mReply_box" v-for="(reply, index) in replies" :key="index">
+                        <div class="mReply_box" v-for="(reply, index) in displayedReplies" :key="index">
                             <div class="mReply">
                                 <span class="n_name">{{ reply.nickName }}</span>
                                 {{ reply.text }}
@@ -94,44 +95,17 @@
             </div>
             <div class="sirenModal" v-if="isModalOpen">
                 <div class="modal-content">
-                    <div id="title">
-                        신고 사유
-                    </div>
+                    <div id="title">신고 사유</div>
                     <div id="wrap">
-                        <div>
-                            <input type="radio" id="abuse" name="siren" value="abuse">
-                            <label for="abuse">욕설, 비방, 차별, 혐오</label>
+                        <div v-for="reason in sirenReasons" :key="reason.id">
+                        <input type="radio" :id="reason.id" :name="reason.name" :value="reason.value" v-model="selectedReason" />
+                        <label :for="reason.id">{{ reason.label }}</label>
                         </div>
-                        <div>
-                            <input type="radio" id="promotion" name="siren" value="promotion">
-                            <label for="promotion">홍보, 영리목적</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="illegal" name="siren" value="illegal">
-                            <label for="illegal">불법정보</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="lewd" name="siren" value="lewd">
-                            <label for="lewd">음란, 청소년 유해</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="personal" name="siren" value="personal">
-                            <label for="personal">개인정보 노출, 금융 거래</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="spam" name="siren" value="spam">
-                            <label for="spam">도배, 스팸</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="etc" name="siren" value="etc" v-model="radioOption">
-                            <label for="etc">기타</label><br>
-                            <input type="text" id="etc" class="etcText" placeholder="신고사유를 적어주세요"
-                            v-if="radioOption === 'etc'">
-                        </div>
+                        <input type="text" class="etcText" v-model="etcReason" v-if="selectedReason === 'etc'" />
                     </div>
                     <div id="bt_wrap">
                         <button class="cancel" @click="closeModal">취소</button>
-                        <button class="registration">등록하기</button>
+                        <button class="registration" @click="submitReport">등록하기</button>
                     </div>
                 </div>
             </div>
@@ -146,6 +120,12 @@ import { useRouter} from 'vue-router';
 const liked = ref(false)
 const isModalOpen = ref(false)
 const router = useRouter();
+const comments = ref([]);
+const commentIndex = ref(0);
+const displayedReplies = ref([]);
+const commentsToShow = 10;
+const selectedReason = ref('');
+const etcReason = ref('');
 
 const toggleLike = () => {
     const ani = document.querySelector('.ani');
@@ -244,16 +224,86 @@ const replies = ref([
         date:'YYYY.MM.DD',
         text:'나도 혹시 자는 중에 이러는거 아닌가 싶어서 무서워지네'
     },
-])
+    {
+        nickName:'로고와스미스',
+        date:'YYYY.MM.DD',
+        text:'저도 진짜 재밌게 봤는데 영화 많이 보신분들한테는 좀 식상할 수도 있음'
+    },
+    {
+        nickName:'동과젤리',
+        date:'YYYY.MM.DD',
+        text:'그럭저럭 나쁘진 않았는데 예고편에 나온 것만큼 대단하진 않았던 걸로'
+    },
+    {
+        nickName:'망고젤리',
+        date:'YYYY.MM.DD',
+        text:'간만에 친구 만나서 같이 봤는데 생각했던 것보다 엄청 재밌어서 놀람 이선균이 이런 연기도 하네'
+    },
+    {
+        nickName:'우웅증말기환자',
+        date:'YYYY.MM.DD',
+        text:'이제 무서워서 잠 어떻게 가는데 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ'
+    },
+    {
+        nickName:'성현박사맘',
+        date:'YYYY.MM.DD',
+        text:'몽유병이 진짜 같이 사는 사람끼리는 이보다 무서운게 없는 병이거든요... 뭔가 남 일 같지 않아서 계속 손에 땀을 쥐면서 봤네요...'
+    },
+    {
+        nickName:'바재부팅부',
+        date:'YYYY.MM.DD',
+        text:'나도 혹시 자는 중에 이러는거 아닌가 싶어서 무서워지네'
+    },
+    {
+        nickName:'로고와스미스',
+        date:'YYYY.MM.DD',
+        text:'저도 진짜 재밌게 봤는데 영화 많이 보신분들한테는 좀 식상할 수도 있음'
+    },
+    {
+        nickName:'동과젤리',
+        date:'YYYY.MM.DD',
+        text:'그럭저럭 나쁘진 않았는데 예고편에 나온 것만큼 대단하진 않았던 걸로'
+    },
+    {
+        nickName:'망고젤리',
+        date:'YYYY.MM.DD',
+        text:'간만에 친구 만나서 같이 봤는데 생각했던 것보다 엄청 재밌어서 놀람 이선균이 이런 연기도 하네'
+    },
+    {
+        nickName:'우웅증말기환자',
+        date:'YYYY.MM.DD',
+        text:'이제 무서워서 잠 어떻게 가는데 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ'
+    },
+    {
+        nickName:'성현박사맘',
+        date:'YYYY.MM.DD',
+        text:'몽유병이 진짜 같이 사는 사람끼리는 이보다 무서운게 없는 병이거든요... 뭔가 남 일 같지 않아서 계속 손에 땀을 쥐면서 봤네요...'
+    },
+    {
+        nickName:'바재부팅부',
+        date:'YYYY.MM.DD',
+        text:'나도 혹시 자는 중에 이러는거 아닌가 싶어서 무서워지네'
+    },
+]);
+
+const sirenReasons = [
+  { id: 'abuse', name: 'siren', value: 'abuse', label: '욕설, 비방, 차별, 혐오' },
+  { id: 'promotion', name: 'siren', value: 'promotion', label: '홍보, 영리목적' },
+  { id: 'illegal', name: 'siren', value: 'illegal', label: '불법정보' },
+  { id: 'lewd', name: 'siren', value: 'lewd', label: '음란, 청소년 유해' },
+  { id: 'personal', name: 'siren', value: 'personal', label: '개인정보 노출, 금융 거래' },
+  { id: 'spam', name: 'siren', value: 'spam', label: '도배, 스팸' },
+  { id: 'etc', name: 'siren', value: 'etc', label: '기타' },
+];
+displayedReplies.value = replies.value.slice(0, commentsToShow);
+
 
 const isLogin = inject('isLogin');
 
 const openSirenPopup = () => {
     if( isLogin.value == true ){
-        // sirenPop.vue를 열고, 부모 창으로부터 메시지를 받을 수 있도록 리스너 설정
         const sirenPopup = window.open('/Siren', '_blank', 'width=450,height=750,top=100px,left=700px');
         window.addEventListener('message', (event) => {
-            // 팝업 창으로부터 메시지를 수신하면 이를 처리합니다.
             console.log('부모 창에서 수신한 메시지:', event.data);
         });
     }else{
@@ -296,59 +346,27 @@ const postDel = () => {
         router.push('/login');
     }
 }
+
+// Reverse the replies array to display them in ascending order by registration date
+const sortedReplies = ref(replies.value.slice().reverse());
+
+// Initialize displayedReplies with the first 10 comments
+displayedReplies.value = sortedReplies.value.slice(0, commentsToShow);
+
+function loadMore() {
+  // Calculate the range of comments to display
+  const startIndex = displayedReplies.value.length;
+  const endIndex = startIndex + commentsToShow;
+
+  // Check if there are more comments to display
+  if (startIndex < sortedReplies.value.length) {
+    // Prepend the next batch of comments to displayedReplies
+    displayedReplies.value = sortedReplies.value.slice(startIndex, endIndex).concat(displayedReplies.value);
+  }
+}
+
 </script>
 
 <style scoped>
 @import url('../assets/css/Rread.css');
-.hidden{display:none;}
-#title{
-    font-size:17px;
-    width:200px;
-    padding: 20px 10px;
-    font-weight: 900;
-}
-#wrap{
-    margin: 0 10px;
-}
-#wrap div{
-    font-size:12px;
-    padding:10px 0;
-    border-bottom:1px solid #ccc;
-}
-#wrap div:first-child{
-    border-top:1px solid #ccc;
-}
-#wrap div label{
-    margin-left:10px;
-}
-.etcText{
-    height:30px;
-    border-radius:30px;
-    outline:0;
-    padding-left:20px;
-    border:1px solid #ccc;
-}
-#bt_wrap{
-    width:100%;
-    display:flex;
-    justify-content:space-evenly;
-    margin:30px 0;
-}
-#bt_wrap button{
-    font-size:12px;
-    font-weight:300;
-    padding:5px 13px;
-    border-radius:10px;
-    cursor: pointer;
-}
-.cancel{
-    background:#fff;
-    color:#B91646;
-    border:1px solid #B91646;
-}
-.registration{
-    background:#B91646;
-    color:#fff;
-    border:0;
-}
 </style>
