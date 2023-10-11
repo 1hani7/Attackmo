9fa2427b2f78dc5c6084e5788eabd37dc911598b
민지 <dlaalswl0313@naver.com>
2023-10-12 00:15:17 +0900

    231012

--- src/main.js
+++ src/main.js
@@ -9,19 +9,6 @@ import router from './router'
 
 import axios from 'axios'; // Axios를 가져옵니다.
 
-// import { initializeApp } from "firebase/app";
-// import "firebase/auth";
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBO4AWYpbBL-7I8mWm9F4VrP-i0O3D-5y0",
-//     authDomain: "pjsecond-4e01a.firebaseapp.com",
-//     projectId: "pjsecond-4e01a",
-//     storageBucket: "pjsecond-4e01a.appspot.com",
-//     messagingSenderId: "18653238368",
-//     appId: "1:18653238368:web:16b5bf548ada141b4f5f05"
-// };
-
-// initializeApp(firebaseConfig); //firebase 초기화
 
 const app = createApp(App)
 var sessionOptions = {
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,166 +1,93 @@
 <template>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-  <div id="wrap">
+  <div id="app" class="wrap">
         <form @submit.prevent="submitForm" id="sign_box">
              <h1>회원가입</h1>
              <div id="email">
                 <label><i class="bi bi-asterisk"></i>이메일</label>
-                <input type="email" name="mail" v-model="formData.mail" id="mail">
+                <input type="email" class="form-control" v-model="email">
              </div>
              <div id="pw">
                 <label><i class="bi bi-asterisk"></i>비밀번호</label>
-                <input type="password" name="password1"  v-model="formData.password1" id="pw">
+                <input type="password" class="form-control"  v-model="password">
              </div>
              <div id="pw_ck">
                 <label><i class="bi bi-asterisk"></i>비밀번호 확인</label>
-                <input type="password" name="password2" v-model="formData.password2">
+                <input type="password" class="form-control" v-model="password2">
                 <p>비밀번호는 6자리 이상의 영문과 숫자에 조합하여야합니다</p>
              </div>
              <div id="nickname">
                 <label><i class="bi bi-asterisk"></i>활동명</label>
-                <input type="text" name="n_name" v-model="formData.n_name" id="aname">
+                <input type="text" class="form-control" name="n_name" id="aname"  v-model="n_name">
              </div>
              <div id="phone">
                 <label><i class="bi bi-asterisk"></i>휴대폰번호</label>
                 <div id="num">
-                  <input type="number" name="number" v-model="formData.number1" id="number1" @input="limitCharacters('number1')" placeholder="010">
-                  -<input type="number" name="number" v-model="formData.number2" id="number2" @input="limitCharacters('number2')">
-                  -<input type="number" name="number" v-model="formData.number3" id="number3" @input="limitCharacters('number3')">
-                  <!-- <input type="number" name="number" v-model="formData.number" id="number" @input="limitCharacters('number')" placeholder="01012341235"> -->
+                  <input type="number" name="number" id="number1" placeholder="010">
+                  -<input type="number" name="number" id="number2" >
+                  -<input type="number" name="number"  id="number3" >
                 </div>       
              </div>
              <div id="button_box">
-                <button type="submit" id="sign" class="bt">가입하기</button>
+                <button type="submit" @click="register" id="sign" class="bt">가입하기</button>
                 <button @click="goBack" type="button" id="cancel" class="bt">취소</button>
              </div>
         </form>
-    </div>
+  </div>
 </template>
 <script>
+import { ref } from "vue";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from "firebase/app";
 import { useRouter } from 'vue-router';
+ 
+  const firebaseConfig = {
+    apiKey: "AIzaSyC_4wWcRfgtT-dVPlL7BsjBMWbO0F2z7xc",
+    authDomain: "attackmo-86940.firebaseapp.com",
+    projectId: "attackmo-86940",
+    storageBucket: "attackmo-86940.appspot.com",
+    messagingSenderId:  "375478701538",
+    appId: "1:375478701538:web:c22eea3ee90ff0b813fdbb"
+  };
+  
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
 export default {
-  setup(){
+  setup() {
+    const email = ref("");
+    const password = ref("");
+    const n_name = ref(""); // 활동명 변수 추가
     const router = useRouter();
+
     const goBack = () => router.go(-1);
-    return {goBack}
-  },  
-  data(){
-    return{
-      formData:{
-        mail:"",
-        password1:"",
-        password2:"",
-        n_name:"",
-        number1:"",
-        number2:"",
-        number3:"",
-        //number:"",
-      },
-      submittedData: [], // 입력된 데이터를 저장할 배열
+
+    const register = async () => {
+      try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email.value, password.value);
+        console.log("등록 성공:", userCredential);
+        alert("가입완료");
+        // 이후 로그인 페이지로 리다이렉트 등의 동작 수행
+        router.push('/'); // 로그인 페이지 경로로 리다이렉트
+      } catch (error) {
+        console.error("등록 실패:", error);
+        alert("가입 실패: " + error.message);
+      }
+    };
+    
+
+    return {
+      email,
+      password,
+      n_name, // 활동명 변수 추가
+      goBack,
+      register,
     };
   },
-  methods: {
-    checkAll() {
-      // const mail = document.forms["sign_box"]["mail"].value;
-      // const password1 = document.forms["sign_box"]["password1"].value;
-      // const password2 = document.forms["sign_box"]["password2"].value;
-      // const n_name = document.forms["sign_box"]["n_name"].value;
-      // const phone = this.formData.number1 + this.formData.number2 + this.formData.number3; // 전화번호 합치기
-      
-      const mail = this.formData.mail;
-      const password1 = this.formData.password1;
-      const password2 = this.formData.password2;
-      const n_name = this.formData.n_name;
-      const phone = this.formData.number1 + this.formData.number2 + this.formData.number3;
-      //const phone = this.formData.number;
-      
-      if (!this.checkMail(mail)) {
-        //console.log("이메일 값: " + this.formData.mail);
-        return false;
-      }
-      if (!this.checkPassword(password1, password2)) {
-        //console.log("비밀번호 값: " + this.formData.password1);
-        //console.log("비밀번호 값: " + this.formData.password2);
-        return false;
-      }
-      if (!this.checkName(n_name)) {
-       // console.log("활동명 값: " + this.formData.n_name);;
-        return false;
-      }
-      if (!this.checkPhone(phone)) {
-        //console.log("휴대폰번호 값: " + this.formData.number1 + this.formData.number2 + this.formData.number3);
-        console.log("휴대폰번호 값: " + this.formData.number);
-        return false;
-      }
-      return true;
-    },
-    checkMail(mail) {
-      // emailRegExp 정의 및 이메일 형식 확인
-      const emailRegExp = /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/;
-      if (!emailRegExp.test(mail)) {
-        alert("이메일 형식이 올바르지 않습니다!");
-        return false;
-      }
-      return true; // 확인이 완료되었을 때
-    },
-    checkPassword(password1, password2) {
-      // 비밀번호 확인
-      if (password1 !== password2) {
-        alert("비밀번호가 맞지않습니다");   
-        return false;
-      }
-      if (password1.length < 6) {
-        alert("비밀번호는 6자리 이상이어야 합니다.");
-        return false;
-      }
-      return true; // 확인이 완료되었을 때
-    },
-    checkName(n_name) {
-      if (!n_name) {
-        alert("활동명을 입력해주세요!");
-        return false;
-      }
-      const nameRegExp = /^[가-힣]{2,4}$/;
-       if (!nameRegExp.test(n_name)) {
-            alert("이름이 올바르지 않습니다.");
-            return false;
-        }
-      return true; // 확인이 완료되었을 때 
-    },
-    checkPhone(phone) {
-      const phoneNumberPattern = /^[0-9]{11}$/;
-      return phoneNumberPattern.test(phone);
-    },
-   
-    submitForm() {
-      if (this.checkAll()) {
-        // 폼 제출 시 formData를 submittedData 배열에 추가
-        this.submittedData.push({
-          mail: this.formData.mail,
-          password1: this.formData.password1,
-          password2: this.formData.password2,
-          n_name: this.formData.n_name,
-          //number: this.formData.phone,
-        });
-        this.formData = {
-            mail: '',
-            password1: '',
-            password2: '',
-            n_name: '',
-            // number1: "",
-            // number2: "",
-            // number3: "",
-            number:"",
-          },
-          console.log(this.submittedData);
-        }
-      },
-    },
 };
 </script>
-
-<style>
-    #wrap{
+<style scoped>
+    .wrap{
         display: flex;
         padding-top: 103px;
         padding-bottom:103px;
@@ -259,7 +186,7 @@ export default {
         margin: 0;
     }
     @media (max-width:1194px) {
-     #wrap,#sign_box,#email,#pw{width:100%;}
+     .wrap,#sign_box,#email,#pw{width:100%;}
      #email input,#pw input,#pw_ck input,#nickname input{width:100%;}
      #pw_ck,#nickname,#phone,#num{width:100%;}
      #pw_ck p{width:100%;font-size: 17px;}
@@ -271,17 +198,16 @@ export default {
     @media (max-width:490px) {
       input{outline:none;}
       label, .bi-asterisk{font-size: 17px;}
-      #wrap{width:calc(100vw - 30px);}
-      #email,#pw{width:90%;}
-      #email input,#pw input,#pw_ck input,#nickname input{width:100%;outline:none;}
-      #pw_ck,#nickname{width:90%;}
-      #pw_ck p{width:100%;font-size:13px;padding:0px;font-size:12px;}
-      #button_box{width:100%;padding:0px;}
-      #button_box button{width:120px;font-size: 17px;}
-      #sign_box{padding:0px;width:90%;}
-      #phone{ width:90%;padding:0px;}
-      #num{gap:0;}  
-      #num input{width:100%;padding:20px;}  
+      .wrap{width:calc(100vw - 30px);
+        padding:0px;margin:0px;}
+      #sign_box,#email,#pw{width:100%;}
+      #email input,#pw input,#pw_ck input,#nickname input{width:100%;}
+      #pw_ck,#nickname,#phone{width:100%;}
+      #pw_ck p{width:100%;font-size:13px;padding:0px;}
+      #button_box{width:100%;}
+      #button_box button{width:130px;font-size: 17px;}
+      #num{margin:0px;padding:0px;width:20%;}
+      #num input{width:70px;}
 }
   
     
