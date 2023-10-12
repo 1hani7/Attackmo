cc9e3d3b8008351b1a38c152b5e2f19f1615b413
dlaalswl0313 <dlaalswl0313@naver.com>
2023-10-12 09:41:49 +0900

    231012

--- src/views/Anotice.vue
+++ src/views/Anotice.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="wrap">
-        <form @submit.prevent="write" id="admin">
+        <form @submit.prevent="uploadData" id="admin">
             <label>작성하기</label>
             <div id="title">
                 <label>제목</label>
@@ -8,8 +8,7 @@
             </div>
             <div class="file_box">
                 <label>첨부파일</label>
-                <input type="file" accept =".gif, .jpg, .png" 
-                name="file" id="file" @change="handleFileChange" >
+                <input type="file" accept =".gif, .jpg, .png" name="file" id="file" @change="handleFileChange" >
                 <p>*gif,jpg,png 선택 가능합니다</p>
             </div>
             <div id="notice">
@@ -17,20 +16,42 @@
                 <textarea v-model="content"></textarea>
             </div>
             <div id="button_box">
-                <button  id="cancel" class="bt">취소</button>
-                <button @click="write" id="check" class="bt">등록하기</button>
+                <button type=button id="cancel" class="bt">취소</button>
+                <button type="submit" id="check" class="bt">등록하기</button>
             </div>
         </form>
     </div>
 </template>
 <script> 
-import data from '../data/data.js'
+// import data from '../data/data.js'
+
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { useRouter } from 'vue-router';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_4wWcRfgtT-dVPlL7BsjBMWbO0F2z7xc",
+  authDomain: "attackmo-86940.firebaseapp.com",
+  projectId: "attackmo-86940",
+  storageBucket: "attackmo-86940.appspot.com",
+  messagingSenderId: "375478701538",
+  appId: "1:375478701538:web:c22eea3ee90ff0b813fdbb"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+const noticeCollection = collection(db, "Anotice");
 
 export default {
+    setup(){
+        const router = useRouter();
+        return {
+            router,
+        };
+    },
     name: 'Create',
     data() {
         return {
-            data: data,
+            // data: data,
             writer: "",
             title: "",
             content: "",
@@ -38,22 +59,37 @@ export default {
         }
     },
     methods: {
-        write() {
-            this.data.push({
-                Number:this.Number,
+        async uploadData() {
+            try {
+                const docRef = await addDoc(noticeCollection, {
                 title: this.title,
-                writer: this.writer,
-                file:this.file,
-            })
-            this.$router.push({
-                path:"/AskMe"
-            })
+                content: this.content,
+                });
+                console.log("Document written with ID: ", docRef.id);
+
+                // 이 부분에서 파일 업로드를 처리하는 로직을 추가
+            // 업로드가 완료되면 알림 표시
+            window.alert("업로드가 완료되었습니다!");
+            } catch (error) {
+                console.error("Error adding document: ", error);
+            }
+            this.router.push('/AskMe');
         },
         handleFileChange(event) {
             // 파일 변경 이벤트를 처리하고 선택한 파일을 저장합니다.
             this.file = event.target.files[0];
         },
-       
+        // write() {
+        //     this.data.push({
+        //         Number:this.Number,
+        //         title: this.title,
+        //         writer: this.writer,
+        //         file:this.file,
+        //     })
+        //     this.$router.push({
+        //         path:"/AskMe"
+        //     })
+        // },
     }
 }      
 </script>
@@ -100,6 +136,7 @@ export default {
         height: 33px;
         background: #fff;
         border-radius: 30px;
+        border:1px solid black;
     }
     #notice{
         display: flex;
@@ -116,6 +153,7 @@ export default {
         width: 100%;
         outline:none;
         padding:20px;
+        font-size:23px;
         
     }
     #button_box{
