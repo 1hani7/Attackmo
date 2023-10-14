5e587d253d9083969a9a221bfb737a24857c9f31
1hani7 <1hani7@naver.com>
2023-10-14 20:37:13 +0900

    sdfsd

--- src/views/MovieMain.vue
+++ src/views/MovieMain.vue
@@ -31,7 +31,7 @@
     </div>
     <div class="div">
       <div class="title">
-        <div class="mainTitle">기대작</div>
+        <div class="mainTitle">상영 예정</div>
         <div class="plusBt">
           <RouterLink to="/Mcoming">+</RouterLink>
         </div>
@@ -139,6 +139,30 @@
         <i @mousedown="slideScrollRight()" class="bi bi-chevron-compact-right"></i>
       </div>
     </div>
+    <div v-if="isLogin" class="div">
+      <div class="title">
+        <div class="mainTitle">기대하는 작품</div>
+        <RouterLink to="/Mark">
+          <div class="plusBt">+</div>
+        </RouterLink>
+      </div>
+      <div class="cont">
+        <i @mousedown="slideScrollLeft()" class="bi bi-chevron-compact-left"></i>
+        <div v-for="(value, key) in bm2" :key="key" class="poster-box">
+          <router-link :to="{ name: 'MovieTitle', query: { movieName: value.제목 } }">
+            <button type="submit">
+              <img class="image" :src="value.포스터" />
+            </button>
+            <div class="view">
+              <div class="movieTitle">{{ value.제목 }}</div>
+              <div class="movieDate">{{ value.개봉일 }}</div>
+            </div>
+            <div class="movieName show">{{ value.제목 }}</div>
+          </router-link>
+        </div>
+        <i @mousedown="slideScrollRight()" class="bi bi-chevron-compact-right"></i>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -152,6 +176,38 @@ export default {
     const coming = JSON.parse(localStorage.getItem('coming'));
     const topTenList = JSON.parse(localStorage.getItem('topTenList')).slice(0, 10);
     const set = JSON.parse(localStorage.getItem('set'));
+    const bookMark = localStorage.getItem('bookmark')==null?'':JSON.parse(localStorage.getItem('bookmark'));
+    const ComingBookmark = localStorage.getItem('ComingBookmark')==null?'':JSON.parse(localStorage.getItem('ComingBookmark'));
+
+
+    // 기대작
+    const exp = reactive([]);
+    const expected = () => {
+      const target = ComingBookmark[ComingBookmark.length-1];
+      let expList = null;
+      let genre = '';
+      for( var i of coming ){
+        if( i.제목 == ' '+target ){
+          genre = i.장르.split(',')[0];
+          break;
+        }
+      }
+      expList = set.filter( (item) => item.장르.indexOf(genre) > -1 );
+
+      const numberOfMoviesToSelect = 12;
+      const randomMovies = [];
+
+      while (randomMovies.length < numberOfMoviesToSelect && expList.length > 0) {
+        const randomIndex = Math.floor(Math.random() * expList.length);
+        randomMovies.push(expList[randomIndex]);
+        expList.splice(randomIndex, 1); // 중복 선택 방지를 위해 이미 선택한 항목은 배열에서 제거
+      }
+
+      exp.value = randomMovies;
+    }
+    onMounted(()=>{
+      expected();
+    })
 
 
     // 추천리스트
@@ -173,26 +229,9 @@ export default {
 
     // 북마크
     const bm = reactive([]);
-    const bookMark = localStorage.getItem('bookmark')==null?'':JSON.parse(localStorage.getItem('bookmark'));
-    const ComingBookmark = localStorage.getItem('ComingBookmark')==null?'':JSON.parse(localStorage.getItem('ComingBookmark'));
     const bookMarkList = () => {
       for( var i of set ){
         for( var j of bookMark ){
-          if( i.제목 == ' '+j ) bm.push(i);
-        }
-      }
-      for( var i of coming ){
-        for( var j of bookMark ){
-          if( i.제목 == ' '+j ) bm.push(i);
-        }
-      }
-      for( var i of set ){
-        for( var j of ComingBookmark ){
-          if( i.제목 == ' '+j ) bm.push(i);
-        }
-      }
-      for( var i of coming ){
-        for( var j of ComingBookmark ){
           if( i.제목 == ' '+j ) bm.push(i);
         }
       }
@@ -200,6 +239,20 @@ export default {
     }
     onMounted(()=>{
       bookMarkList();
+    })
+
+    // 기대하는 작품
+    const bm2 = reactive([]);
+    const bookMarkList2 = () => {
+      for( var i of coming ){
+        for( var j of ComingBookmark ){
+          if( i.제목 == ' '+j ) bm2.push(i);
+        }
+      }
+      return ;
+    }
+    onMounted(()=>{
+      bookMarkList2();
     })
 
 
@@ -262,7 +315,7 @@ export default {
     return {
       isLogin, topTenList, titleModal, now,
       slideScrollRight, slideScrollLeft, bookMark, bookMarkList,
-      bm, randomMovies, rec
+      bm, bm2, randomMovies, rec, exp
     }
   }
 }
